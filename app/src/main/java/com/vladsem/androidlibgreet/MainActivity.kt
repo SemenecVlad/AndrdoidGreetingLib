2b1a42fdfce2c8c314c26232a0fa131d27778ee0
Vladislav <admin777@MacBook-Pro-Vladislav.local>
2023-04-21 15:00:15 +0300

    Initial

---
+++ app/src/main/java/com/vladsem/androidlibgreet/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.vladsem.androidlibgreet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vladsem.androidlibgreet.ui.theme.AndroidLibGreetTheme
+import com.vladsem.greet_lib.Greet
+
+val greetLib = Greet.getInstance()
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidLibGreetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = greetLib.sayHello(name),
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidLibGreetTheme {
+        Greeting("Android")
+    }
+}
---
+++ greet-lib/src/main/java/com/vladsem/greet_lib/Greet.kt
@@ -0,0 +1,25 @@
+package com.vladsem.greet_lib
+
+import android.annotation.SuppressLint
+import android.content.Context
+
+class Greet {
+    var initialized = false
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        private var instance: Greet? = null
+
+        @Synchronized
+        fun getInstance(): Greet {
+            if (instance == null) {
+                instance = Greet()
+            }
+            return instance!!
+        }
+    }
+
+    fun sayHello(name: String): String {
+        return "Hello $name"
+    }
+}
